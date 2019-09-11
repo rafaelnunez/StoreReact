10eb1e5a3f1b037cdd8ca6f6a36e3dec9269f101
Rafael Nunez <rafael.nunez@alegra.com>
2019-09-10 23:06:59 -0500

    Creando componentes

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../componets/Header';
+import Search from '../componets/Search';
+import '../assets/styles/App.scss';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Search />
+  </div>
+);
+
+export default App;
