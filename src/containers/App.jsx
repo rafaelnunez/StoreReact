68cb0121286f4985f47ba148cad139bec136e986
Rafael Nunez <rafael.nunez@alegra.com>
2019-09-13 00:10:55 -0500

    adelanto

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../componets/Header';
 import Search from '../componets/Search';
+import Categories from '../componets/Categories';
+import Carousel from '../componets/Carousel';
+import CarouselItem from '../componets/CarouselItem';
+import Footer from '../componets/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+    <Footer />
   </div>
 );
 
